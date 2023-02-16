b668c3722bbfca2f5418b760ef169f11868c7d2a
Kostiantyn1980 <Thecaty1980@gmail.com>
2023-02-15 19:27:39 -0700

    first commit

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,21 @@
+describe('template spec', () => {
+
+// cypress.on('ucaught:exception', (err,runnable) => {
+// // returning false here prevents Cypress from
+// // failing the test
+
+  it('navigate to main page', () => {
+    cy.visit('https://www.accesstravel.com/en-US')
+
+
+
+
+
+
+
+
+
+
+    
+  })
+})
