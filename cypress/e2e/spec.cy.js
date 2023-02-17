f678c90e4a6fbd75b8ef2c56548920c7207431a6
Kostiantyn1980 <Thecaty1980@gmail.com>
2023-02-16 18:50:02 -0700

    assignment#5

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,10 +1,10 @@
 describe('template spec', () => {
 
-// cypress.on('ucaught:exception', (err,runnable) => {
-// returning false here prevents Cypress from
-// failing the test
+  // cypress.on('ucaught:exception', (err,runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
 
-it('navigate to main page', () => {
+  it('navigate to main page', () => {
     cy.visit('https://www.accesstravel.com/en-US/Home/Index')
     cy.url().should('eq', 'https://www.accesstravel.com/en-US/Home/Index')
   })
@@ -14,33 +14,40 @@ it('navigate to main page', () => {
 
   })
 
-    it('main page sanity test button Hotel', () => {
-      // cy.visit('https://www.accesstravel.com/en-US/Home/Index')
-      cy.get('[class="hotels"]').should("be.visible")     
-      cy.get('[class="hotels"]').click()
+  it('main page sanity test button Hotel', () => {
+    // cy.visit('https://www.accesstravel.com/en-US/Home/Index')
+    cy.get('[class="hotels"]').should("be.visible")
+    cy.get('[class="hotels"]').click()
+    cy.url().should('eq', 'https://www.accesstravel.com/en-US/Hotel/List')
+  })
 
-    })
+  it('main page sanity test button Guides', () => {
+    cy.get('[class="guides js-list-guides"]').should("be.visible")
+    cy.get('[class="guides js-list-guides"]').click()
+    cy.url().should('eq', 'https://www.accesstravel.com/en-US/Guide/List?DestinationId=2')
+  })
 
-    it('main page sanity test button Guides', () => {
-      cy.get('[class="guides js-list-guides"]').should("be.visible")
-      cy.get('[class="guides js-list-guides"]').click()
-    })
+  it('main page sanity test button Tours', () => {
+    cy.get('[class="tours js-list-tours"]').should("be.visible")
+    cy.get('[class="tours js-list-tours"]').click()
+    cy.url().should('eq', 'https://www.accesstravel.com/en-US/Tour/List?DestinationId=2')
+  })
 
-    it('main page sanity test button Tours', () => {
-      cy.get('[class="tours js-list-tours"]').should("be.visible")
-      cy.get('[class="tours js-list-tours"]').click()
+  it('main page sanity test button Things to do', () => {
+    cy.get('[class="tours attraction-link"]').should("be.visible")
+    cy.get('[class="tours attraction-link"]').click()
+    cy.url().should('eq', 'https://www.accesstravel.com/en-US')
 
-    })
+  })
+  it('main page sanity test Login button', () => {
+    cy.contains('Login').click()    
+    cy.url().should('eq', 'https://www.accesstravel.com/en-US/Account/Login')
+  })
 
-    it('main page sanity test button Things to do', () => {
-cy.get('[class="tours attraction-link"]').should("be.visible")
-cy.get('[class="tours attraction-link"]').click()
-
-
-     
-
-
-
+  it('main page sanity test Signup button', () => {
+    cy.contains('Signup').click()    
+    cy.url().should('eq', 'https://www.accesstravel.com/en-US/Account/Register')
     
+
   })
 })
