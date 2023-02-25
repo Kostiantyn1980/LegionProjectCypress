b2a5b6fd3a2c5c19742b43eb069790d035bea31a
Kostiantyn1980 <Thecaty1980@gmail.com>
2023-02-24 19:01:23 -0700

    update

--- cypress/e2e/Legion_Assignment_test2.cy.js
+++ cypress/e2e/Legion_Assignment_test2.cy.js
@@ -15,19 +15,19 @@ describe('template spec', () => {
 
   })
   it('positive test search button Jerusalem', () => {
-    cy.get('[class="form-control city-select"]').type('Jerusalem')
+    cy.get('[class="form-control city-select"]').type('Jerusalem').should("be.visible")
     cy.get('[class="form-control city-select"]').select('Jerusalem') // Select the 'Jerusalem' option
     cy.get('[type="submit"]').click()
 
   })
   it('positive test search button London', () => {
-    cy.get('[class="form-control city-select"]').type('London')
+    cy.get('[class="form-control city-select"]').type('London').should("be.visible")
     cy.get('[class="form-control city-select"]').select('London') // Select the 'London' option
     cy.get('[type="submit"]').click()
 
   })
   it('positive test search button New York', () => {
-    cy.get('[class="form-control city-select"]').type('New York')
+    cy.get('[class="form-control city-select"]').type('New York').should("be.visible")
     cy.get('[class="form-control city-select"]').select('New York') // Select the 'New York' option
     cy.get('[type="submit"]').click()
 
@@ -36,8 +36,8 @@ describe('template spec', () => {
     cy.get('[data-val-required="The ChildrenNum field is required."]').clear()
     cy.get('[data-val-required="The ChildrenNum field is required."]').type('2').should('have.value','2')
     cy.get('[type="submit"]').click()
-    cy.get('[id="Filter_ChildrensAge[0]"]').clear().type('10').click()
-    cy.get('[id="Filter_ChildrensAge[1]"]').clear().type('12').click()
+    cy.get('[id="Filter_ChildrensAge[0]"]').clear().type('10').click().should('have.value','10')
+    cy.get('[id="Filter_ChildrensAge[1]"]').clear().type('12').click().should('have.value','12')
   })
 
   it('negative dates test Checkin', () => {
@@ -47,7 +47,7 @@ describe('template spec', () => {
   })
   it('negative dates test Checkout', () => {
     cy.get('[name="Filter.CheckOut"]').clear()
-    cy.get('[name="Filter.CheckOut"]').type('1111-06-14').should('have.value', '1111-06-14')
+    cy.get('[name="Filter.CheckOut"]').type('14-2001-21').should('have.value', '14-2001-21')
     cy.get('[type="submit"]').click()
 
   })
